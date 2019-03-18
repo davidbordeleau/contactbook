4279249e5d9938cf58fb95b540f1886072046f9c
David Bordeleau <davidbordeleau@hotmail.com>
2019-03-18 15:21:44 +0100

    add search possibilities

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -3,7 +3,18 @@ class ContactsController < ApplicationController
     if Contact.all.empty?
       Contact.load_csv
     end
+    sql_query = "name ILIKE :name OR email ILIKE :email"
+
+    @contacts = Contact.where(sql_query, name: params[:name], email: params[:email])
+
+    if params[:query].present?
+      sql_query = "name ILIKE :query OR email ILIKE :query OR cast(phone as text) ILIKE :query"
+
+      @contacts = Contact.where(sql_query, query: params[:query])
+    elsif !params[:query].present? && !params[:name].present? && !params[:email].present?
       @contacts = Contact.all
+    end
+
   end
 
   def new
