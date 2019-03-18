a771c336ea7f2cd8a2aecb0cad0465173d7fdf6d
David Bordeleau <davidbordeleau@hotmail.com>
2019-03-18 12:33:25 +0100

    basic reading csv file

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,2 +1,23 @@
 class ContactsController < ApplicationController
+  def index
+    if Contact.all.empty?
+      Contact.load_csv
+    end
+      @contacts = Contact.all
+  end
+
+  def new
+    @contact = Contact.new
+  end
+
+  def create
+    @contact = Contact.new(contact_params)
+    @contact.save
+  end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:name, :email, :phone)
+  end
 end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,13 @@
+require 'csv'
+
 class Contact < ApplicationRecord
+
+
+  private
+
+  def self.load_csv
+    CSV.foreach(Rails.root.join('db/contacts.csv'), headers: true) do |row|
+      Contact.create(name: row[0], email: row[8], phone: row[11])
+    end
+  end
 end
